6b6b76bc2215ac44dd70e8bd1a57cd318414c424
Aznan <aznankhan1234567@gmail.com>
2020-06-04 22:24:32 +0530

    Component part 1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from "reactstrap";
 import './App.css';
+import Menu from "./components/MenuComponent"
 import { render } from 'react-dom';
 
-function App() {
-  // render() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-        <NavbarBrand href="https://coolcampsites.herokuapp.com/" >KHAN'S RESTORUANTA</NavbarBrand>
-        </div> 
-      </Navbar>
-    </div>
-  );
-  // }
+class App extends Component{
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+          <NavbarBrand href="https://coolcampsites.herokuapp.com/" >KHAN'S RESTORUANTA</NavbarBrand>
+          </div> 
+        </Navbar>
+        <Menu />
+      </div>
+    );
+  }
 }
 
 export default App;
