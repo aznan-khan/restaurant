7b75809a3ec90adaaf1e2bf14b9170f6fc7561cd
AznanKhan <aznankhan1234567@gmail.com>
2020-08-01 01:23:38 +0530

    Single Page Application Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -76,7 +76,8 @@
 
 
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 class Dishdetail extends Component {
 
@@ -118,12 +119,22 @@ class Dishdetail extends Component {
         if (this.props.dish) {
             return (
                 <div className="container">
+                    <div className='row'>
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{this.props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{this.props.dish.name}</h3>
+                            <hr/>
+                        </div>
+                    </div>
                     <div className="row">
                         <div className="col-12 col-md-5 m-1">
                             {this.renderDish(this.props.dish)}
                         </div>
                         <div className="col-12 col-md-5 m-1">
-                            {this.renderComments(this.props.dish.comments)}
+                            {this.renderComments(this.props.comments)}
                         </div>
                     </div>
                 </div>
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -38,8 +38,8 @@
 
 
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import {Link} from 'react-router-dom';
 class Menu extends Component {
 
     render() {
@@ -47,10 +47,12 @@ class Menu extends Component {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
                     <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{dish.name}</CardTitle>
-                        </CardImgOverlay>
+                        <Link to={`/menu/${dish.id}`}>
+                            <CardImg width="100%" src={dish.image} alt={dish.name} />
+                            <CardImgOverlay>
+                                <CardTitle>{dish.name}</CardTitle>
+                            </CardImgOverlay>
+                        </Link>
                     </Card>
                 </div>
             )
@@ -58,6 +60,17 @@ class Menu extends Component {
 
         return (
             <div className="container">
+                <div className='row'>
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr/>
+                    </div>
+                    
+                </div>
                 <div className="row">
                     {menu}
                 </div>
